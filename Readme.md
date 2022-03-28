6553bdcfef6a619afc9eb430f28962611e4ebe20
Alec Helbling <alechelbling@Alecs-MacBook-Pro-2.local>
2022-03-27 23:50:29 -0400

    Logo update

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # Manim Machine Learning
 
-Manim Machine Learning is a project focused on providing animations and visualizations of common machine learning concepts with the [Manim Community Library](https://www.manim.community/). We want this project to be a compilation of primitive visualizations that can be easily combined to create videos about complex machine learning concepts. 
+Manim Machine Learning is a project focused on providing animations and visualizations of common machine learning concepts with the [Manim Community Library](https://www.manim.community/). We want this project to be a compilation of primitive visualizations that can be easily combined to create videos about complex machine learning concepts. Additionally, we want to provide a set of abstractions which allow users to focus on explanations instead of software engineering.
 
 <img src="examples/ManimMLLogo.gif">
 
--- src/logo.py
+++ src/logo.py
@@ -4,7 +4,7 @@
 from manim import *
 from neural_network import NeuralNetwork
 
-config.pixel_height = 720 
+config.pixel_height = 500 
 config.pixel_width = 1920 
 config.frame_height = 10.0
 config.frame_width = 10.0
@@ -13,6 +13,7 @@ class ManimMLLogo(Scene):
 
     def construct(self):
         self.text = Text("ManimML")
+        self.text.scale(1.3)
         self.neural_network = NeuralNetwork([3, 5, 3, 6, 3], layer_spacing=0.6, node_color=BLUE)
         self.neural_network.scale(0.8)
         self.neural_network.move_to(self.text.get_right())
@@ -22,10 +23,12 @@ class ManimMLLogo(Scene):
         self.logo_group.move_to(ORIGIN)
         self.play(Write(self.text))
         self.play(Create(self.neural_network))
-        self.surrounding_rectangle = SurroundingRectangle(self.logo_group, buff=0.3, color=BLUE)
+        # self.surrounding_rectangle = SurroundingRectangle(self.logo_group, buff=0.3, color=BLUE)
+        underline = Underline(self.text, color=BLUE)
         animation_group = AnimationGroup(
             self.neural_network.make_forward_propagation_animation(run_time=5),
-            Create(self.surrounding_rectangle)
+            Create(underline),
+        #    Create(self.surrounding_rectangle)
         )
         self.play(animation_group)
         self.wait(5)
