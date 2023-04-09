abba4730ee3a01fc37de900bfb25599c8489b79d
Lawreros <lawreros@gmail.com>
2023-04-09 01:47:27 +0000

    Working version of transposed convolutional layer

--- manim_ml/neural_network/layers/convolutional_2d_to_trans_conv_2d.py
+++ manim_ml/neural_network/layers/convolutional_2d_to_trans_conv_2d.py
@@ -39,27 +39,25 @@ class Convolutional2DToTransConv2D(Convolutional2DToConvolutional2D,ConnectiveLa
     def make_forward_pass_animation(self, layer_args={}, run_time=1.5, **kwargs):
         """Forward pass animation from conv2d to transposed conv2d"""
         
+        new_input_layer_feature_map_size= (
+            self.input_layer.feature_map_size[0] * (self.output_layer.in_padding[0]+1) + self.output_layer.in_padding[0],
+            self.input_layer.feature_map_size[1] * (self.output_layer.in_padding[1]+1) + self.output_layer.in_padding[1],
+        )
+
         #Replace previous mobject with something
 
         grid_stroke_width = 1
         cell_width = self.input_layer.cell_width
-        kernel_size = self.output_layer.kernel_size
-
-        # Get the untransformed height and width of the input feature maps
-        nheight = self.input_layer.feature_maps.submobjects[0].exterior_rectangle.untransformed_height
-        nwidth = self.input_layer.feature_maps.submobjects[0].exterior_rectangle.untransformed_width
 
         new_feature_animation = []
 
         for feature_map_index, feature_map in enumerate(self.input_layer.feature_maps):
             gridded_rectangle = GriddedRectangle(
                 color=self.active_color,
-                    # height=self.input_layer.feature_map_size[1] * cell_width * (self.output_layer.padding[1]+1),
-                    # width=self.input_layer.feature_map_size[0] * cell_width * (self.output_layer.padding[0]+1),
-                    height=nheight * (self.output_layer.padding[1]+1) + cell_width*self.output_layer.padding[1],
-                    width=nwidth * (self.output_layer.padding[0]+1) + cell_width*self.output_layer.padding[0],
-                    grid_xstep=cell_width,# * kernel_size,
-                    grid_ystep=cell_width,# * kernel_size,
+                    height = new_input_layer_feature_map_size[1]*cell_width,
+                    width = new_input_layer_feature_map_size[0]*cell_width,
+                    grid_xstep=cell_width,
+                    grid_ystep=cell_width,
                     grid_stroke_width=grid_stroke_width,
                     grid_stroke_color=self.active_color,
                     show_grid_lines=True,
@@ -69,8 +67,8 @@ class Convolutional2DToTransConv2D(Convolutional2DToConvolutional2D,ConnectiveLa
             # 2. Randomly highlight one of the cells in the kernel.
             highlighted_cells = []
 
-            for kernel_x in range(self.output_layer.padding[0], self.input_layer.feature_map_size[0]*(self.output_layer.padding[0]+1), self.output_layer.padding[0]+1):
-                    for kernel_y in range(self.output_layer.padding[1], self.input_layer.feature_map_size[1]*(self.output_layer.padding[1]+1), self.output_layer.padding[1]+1):
+            for kernel_x in range(self.output_layer.in_padding[0], self.input_layer.feature_map_size[0]*(self.output_layer.in_padding[0]+1), self.output_layer.in_padding[0]+1):
+                    for kernel_y in range(self.output_layer.in_padding[1], self.input_layer.feature_map_size[1]*(self.output_layer.in_padding[1]+1), self.output_layer.in_padding[1]+1):
                         cell_rectangle = GriddedRectangle(
                             color=self.active_color,
                             height=cell_width,
@@ -97,6 +95,15 @@ class Convolutional2DToTransConv2D(Convolutional2DToConvolutional2D,ConnectiveLa
                         cell_rectangle.shift(kernel_shift_vector)
                         highlighted_cells.append(cell_rectangle)
 
+
+            qqq = gridded_rectangle.copy()
+            qqq.set_opacity(0)
+            self.input_layer.feature_maps[feature_map_index] = qqq.rotate(
+                ThreeDLayer.rotation_angle,
+                about_point=qqq.get_center(),
+                axis=ThreeDLayer.rotation_axis,
+            ).move_to(feature_map.get_center())
+
             # Rotate the gridded rectangles so they match the angle
             # of the conv maps
             gridded_rectangle_group = VGroup(gridded_rectangle, *highlighted_cells)
@@ -118,8 +125,9 @@ class Convolutional2DToTransConv2D(Convolutional2DToConvolutional2D,ConnectiveLa
 
         
         # Change the feature map size of the input layer to account for padding
-        self.input_layer.feature_map_size = nheight * (self.output_layer.padding[1]+1)
-        # self.output_layer.feature_map_size = nwidth * (self.output_layer.padding[0]+1)
+        self.input_layer.feature_map_size = new_input_layer_feature_map_size
+        
+        self.feature_map_size = new_input_layer_feature_map_size
         
         # Call on the super class to make the animation for the conv2d to conv2d now that you
         # have the padded feature maps
--- manim_ml/neural_network/layers/trans_conv_2d.py
+++ manim_ml/neural_network/layers/trans_conv_2d.py
@@ -5,7 +5,7 @@ from manim_ml.neural_network.layers.parent_layers import (
     ThreeDLayer,
     VGroupNeuralNetworkLayer,
 )
-
+from manim_ml.neural_network.layers.convolutional_2d import FeatureMap
 
 class TransposeConvolution2DLayer (VGroupNeuralNetworkLayer, ThreeDLayer):
     """Transpose convolution layer for Convolutional2DLayer"""
@@ -15,7 +15,9 @@ class TransposeConvolution2DLayer (VGroupNeuralNetworkLayer, ThreeDLayer):
             num_feature_maps,
             feature_map_size=None,
             filter_size=None,
-            pad=1,
+            in_pad=1,
+            padding=0,
+            padding_dashed=True,
             kernel_size=3,
             stride=1,
             cell_highlight_color=ORANGE,
@@ -25,6 +27,7 @@ class TransposeConvolution2DLayer (VGroupNeuralNetworkLayer, ThreeDLayer):
             active_color=ORANGE,
             filter_color=ORANGE,
             show_grid_lines=False,
+            fill_opacity=0.3,
             stroke_width=2.0,
             **kwargs
     ):
@@ -39,6 +42,24 @@ class TransposeConvolution2DLayer (VGroupNeuralNetworkLayer, ThreeDLayer):
         """
 
         super().__init__(**kwargs)
+
+        if isinstance(padding, tuple):
+            assert len(padding) == 2
+            self.padding = padding
+        elif isinstance(padding, int):
+            self.padding = (padding, padding)
+        else:
+            raise Exception(f"Unrecognized type for padding: {type(padding)}")
+
+        # Add check for internal padding of transpose convolution
+        if isinstance(in_pad, tuple):
+            assert len(in_pad) == 2
+            self.in_padding = in_pad
+        elif isinstance(in_pad, int):
+            self.in_padding = (in_pad, in_pad)
+        else:
+            raise Exception(f"Unrecognized type for padding: {type(in_pad)}")
+
 
         if isinstance(feature_map_size, int):
             self.feature_map_size = (feature_map_size, feature_map_size)
@@ -61,7 +82,8 @@ class TransposeConvolution2DLayer (VGroupNeuralNetworkLayer, ThreeDLayer):
         self.filter_color = filter_color
         self.show_grid_lines = show_grid_lines
         self.stroke_width = stroke_width
-        self.padding = (pad,pad)
+        self.fill_opacity = fill_opacity
+        self.padding_dashed = padding_dashed
 
     def construct_layer(
             self,
@@ -70,50 +92,45 @@ class TransposeConvolution2DLayer (VGroupNeuralNetworkLayer, ThreeDLayer):
             **kwargs
     ):
         # Make the output feature maps
-        self.feature_maps = self._make_output_feature_maps(
-            input_layer.num_feature_maps, input_layer.feature_map_size
-        )
+        # self.feature_maps = self._make_output_feature_maps(
+        #     input_layer.num_feature_maps, input_layer.feature_map_size
+        # )
+
+        self.feature_maps = self.construct_feature_maps(input_layer)
+
         self.add(self.feature_maps)
         self.rotate(
             ThreeDLayer.rotation_angle,
             about_point=self.get_center(),
             axis=ThreeDLayer.rotation_axis,
         )
-        # TODO: Calculate feature map size resulting from transpose convolution
-        self.feature_map_size = (
-            input_layer.feature_map_size[0] / self.kernel_size,
-            input_layer.feature_map_size[1] / self.kernel_size,
-        )
         super().construct_layer(input_layer, output_layer, **kwargs)
 
-    def _make_output_feature_maps(self, num_input_feature_maps, input_feature_map_size):
-        """Makes a set of output feature maps"""
-        # Compute the size of the feature maps
-        output_feature_map_size = (
-            input_feature_map_size[0]*self.padding[0] / self.kernel_size,
-            input_feature_map_size[1]*self.padding[1] / self.kernel_size,
-        )
+
+    def construct_feature_maps(self, input_layer):
+        """Creates the neural network layer"""
         # Draw rectangles that are filled in with opacity
         feature_maps = []
-        for filter_index in range(num_input_feature_maps):
-            rectangle = GriddedRectangle(
+
+        feature_map_size = (
+            (input_layer.feature_map_size[0] * (self.in_padding[0]+1) + self.in_padding[0] - self.filter_size[0] + 1)/ self.stride,
+            (input_layer.feature_map_size[1] * (self.in_padding[1]+1) + self.in_padding[1] - self.filter_size[1] + 1)/ self.stride,
+        )
+
+        for filter_index in range(self.num_feature_maps):
+            feature_map = FeatureMap(
                 color=self.color,
-                height=output_feature_map_size[1] * self.cell_width,
-                width=output_feature_map_size[0] * self.cell_width,
+                feature_map_size=feature_map_size,
+                cell_width=self.cell_width,
                 fill_color=self.color,
-                fill_opacity=0.2,
-                stroke_color=self.color,
-                stroke_width=self.stroke_width,
-                grid_xstep=self.cell_width,
-                grid_ystep=self.cell_width,
-                grid_stroke_width=self.stroke_width / 2,
-                grid_stroke_color=self.color,
-                show_grid_lines=self.show_grid_lines,
+                fill_opacity=self.fill_opacity,
+                padding=self.padding,
+                padding_dashed=self.padding_dashed,
             )
             # Move the feature map
-            rectangle.move_to([0, 0, filter_index * self.filter_spacing])
+            feature_map.move_to([0, 0, filter_index * self.filter_spacing])
             # rectangle.set_z_index(4)
-            feature_maps.append(rectangle)
+            feature_maps.append(feature_map)
 
         return VGroup(*feature_maps)
 
