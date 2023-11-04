e1d9db27c6f2d10f5ba85b34c5c29e3120a406d7
Alec Helbling <alechelbling@gatech.edu>
2023-11-04 14:32:22 -0400

    Update parent_layers.py

--- manim_ml/neural_network/layers/parent_layers.py
+++ manim_ml/neural_network/layers/parent_layers.py
@@ -7,8 +7,11 @@ class NeuralNetworkLayer(ABC, Group):
     def __init__(self, text=None, *args, **kwargs):
         super(Group, self).__init__()
         self.title_text = kwargs["title"] if "title" in kwargs else " "
-        self.title = Text(self.title_text, font_size=DEFAULT_FONT_SIZE / 3).scale(0.6)
-        self.title.next_to(self, UP, 1.2)
+        if "title" in kwargs:
+            self.title = Text(self.title_text, font_size=DEFAULT_FONT_SIZE / 3).scale(0.6)
+            self.title.next_to(self, UP, 1.2)
+        else:
+            self.title = Group()
         # self.add(self.title)
 
     @abstractmethod
